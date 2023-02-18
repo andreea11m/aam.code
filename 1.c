e6ab3a39b221e1150ad7fced79c2a42c11d5815b
andreea11m <51531448+andreea11m@users.noreply.github.com>
2023-02-18 22:52:59 +0200

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//@aam.codes
+
+int main () {
+
+    int a;
+    printf ("a = ");
+    scanf ("%d", &a);
+    printf ("the number %d\n", a);
+    printf ("base 10 : %d\n", a);
+    printf ("base 8 : %o\n", a);
+    printf ("base 16 : %x\n", a);
+    printf ("\tby @aam.codes");
+
+    return 0;
+}
+
+
+
+
+//Write a program that reads an integer from the keyboard and displays the number in base 10, base 8, and base 16.
+//Scrieţi un program care citeşte un număr întreg de la tastatură şi afişează numărul în baza 10, baza 
+//8 şi baza 16.
